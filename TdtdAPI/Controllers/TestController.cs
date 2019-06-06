56fafab8868468bc47220ad9f2c8a0271b9048b4
Cube <thefabulouscube@tadaatiedye.com>
2019-06-05 21:40:36 -0400

    Cleaning DeployToGoDaddy.pubxml

--- TdtdAPI/Controllers/TestController.cs
+++ TdtdAPI/Controllers/TestController.cs
@@ -19,28 +19,13 @@ namespace TdtdAPI.Controllers
         {
             _repoWrapper = repoWrapper;
         }
-        //GET api/test
-        //[HttpGet]
-        // public IEnumerable<Magnets> Get()
-        // {
-        //     var magnets = _repoWrapper.Magnet.FindAll();
-
-        //     //_logger.LogInfo("Here is info QA message from our values controller.");
-        //     //_logger.LogDebug("Here is debug QA message from our values controller.");
-        //     //_logger.LogWarn("Here is warn QA message from our values controller.");
-        //     //_logger.LogError("Here is error QA message from our values controller.");
-
-        //     return magnets;
-        // }
+ 
         [HttpGet]
         public IHttpActionResult Getwithresp()
         {
             var magnets = _repoWrapper.Magnets.FindAll();
 
-            //_logger.LogInfo("Here is info QA message from our values controller.");
-            //_logger.LogDebug("Here is debug QA message from our values controller.");
-            //_logger.LogWarn("Here is warn QA message from our values controller.");
-            //_logger.LogError("Here is error QA message from our values controller.");
+            //_logger.LogInfo("Here is info message from our values controller.");
 
             return Ok(magnets);
         }
@@ -50,10 +35,7 @@ namespace TdtdAPI.Controllers
         {
             var magnet = _repoWrapper.Magnets.GetMagnetById(id);
 
-            //_logger.LogInfo("Here is info QA message from our values controller.");
-            //_logger.LogDebug("Here is debug QA message from our values controller.");
-            //_logger.LogWarn("Here is warn QA message from our values controller.");
-            //_logger.LogError("Here is error QA message from our values controller.");
+            //_logger.LogInfo("Here is info message from our values controller.");
 
             return Ok(magnet);
         }
